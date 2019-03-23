dacdd61ff7fe716c147eb88e774e8a6f90e834f6
jplaceres <33101350+jplaceres@users.noreply.github.com>
2019-03-23 17:04:07 -0400

    created client.py

---
+++ server/src/client.py
@@ -0,0 +1,13 @@
+#client side
+
+from google.cloud import storage
+# If you don't specify credentials when constructing the client,
+# library will look for credentials in the environment.
+storage_client = storage.Client()
+
+# Make an authenticated API request
+buckets = list(storage_client.list_buckets())
+print(buckets)
+
+# [END auth_cloud_implicit]
+
