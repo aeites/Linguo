a547b431f9f8a68fc0dc0f2791739e1420833fc3
aeites <er.t.chan@gmail.com>
2019-03-23 21:35:16 -0400

    Made the vision library into a class

---
+++ server/src/resources/vision-handler.py
@@ -0,0 +1,41 @@
+import io
+import os
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+class VisionHandler:
+    def __init__(self, credentials_path):
+        self.authenticate(credentials_path)
+
+        # Instantiates a client
+        self.client = vision.ImageAnnotatorClient()
+
+    def authenticate(self, credentials_path):
+        # Set the path to the credentials
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+    # return meta data of the image
+    def process_image(self, image_path):
+        # The name of the image file to annotate
+        file_name = os.path.join(os.path.dirname(__file__),
+                                 image_path)
+        # Loads the image into memory
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+
+        # Performs label detection on the image file
+        response = self.client.label_detection(image=image)
+        labels = response.label_annotations
+
+        print('Labels:')
+        for label in labels:
+            print(label.description)
+
+
+vh = VisionHandler(r"D:\Users\Chana-PC\Documents\Linguo\vision-key.json")
+vh.process_image(r"D:\Users\Chana-PC\Documents\Linguo\server\src\resources\wakeupcat.jpg")
+
+
--- server/src/server.py
+++ server/src/server.py
@@ -6,7 +6,7 @@ from google.cloud.vision import types
 class Server:
     def __init__(self):
         self.api_key = self.get_api_key()
-    
+
     #method to get files from the bucket
     def getUnprocessedFiles(self):
         # returns the file names of things to process
@@ -20,33 +20,19 @@ class Server:
         images = self.getUnprocessedFiles()
 
 
-
-        # sending get request and saving the response as response object
-        r = requests.get(url=URL, params=PARAMS)
-
-        # extracting data in json format
-        data = r.json()
-
-        # extracting latitude, longitude and formatted address
-        # of the first matching location
-        latitude = data['results'][0]['geometry']['location']['lat']
-        longitude = data['results'][0]['geometry']['location']['lng']
-        formatted_address = data['results'][0]['formatted_address']
-
-        # printing the output
-        print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-              % (latitude, longitude, formatted_address))
-
         print("Unsupported Method")
 
     # Method to draw the text onto the image
     def drawText(self, image, coordinates, text):
         print("Unsupported Method")
 
-    def get_api_key(self):
-        f = open("API_Key.txt", "r")
-        return f.read()
+
+    def set_api_key(self):
+
+
+
     # find way to use translate API
+
 
 
 s = Server()
