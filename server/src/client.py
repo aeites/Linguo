d4b77b7833c1ccebb400911bebf29878fa38bfd1
jplaceres <33101350+jplaceres@users.noreply.github.com>
2019-03-24 01:57:31 -0400

    make client class

--- server/src/client.py
+++ server/src/client.py
@@ -2,30 +2,38 @@
 from google.cloud import storage
 from google.cloud.storage import Blob
 
-
-
 # If you don't specify credentials when constructing the client,
 # library will look for credentials in the environment.
 
-storage_client = storage.Client.from_service_account_json('Linguo-495a24a54222.json')
+class StorageHandler:
+    def __init__(self, jsonfile):
+        self.storage_client = storage.Client.from_service_account_json(jsonfile)        
+        
+    def defineBucket(self):
+        bucketslist = list(self.storage_client.list_buckets())
+        bucket = self.storage_client.get_bucket("storage_photos")
+        return bucket
 
-# Make an authenticated API request
-bucketslist = list(storage_client.list_buckets())
-bucket = storage_client.get_bucket("storage_photos")
-blobs = bucket.list_blobs()
+    def defineBlobs(self, bucket):
+        blobs = bucket.list_blobs()
+        return blobs
 
-x=1
-for blob in blobs:
-    print(blob.name)
-    #filename = "blob" + str(x) + ".jpeg"
-    blob.download_to_filename(blob.name)
-    x=x+1
+    def downloadAllBlobs(self, blobs):
+        for blob in blobs:
+            print(blob.name)
+            blob.download_to_filename(blob.name)
+            
+    def uploadNewPhoto(self, file, bucket):
+        blob = Blob(file, bucket)
+        with open(file, "rb") as my_file:
+            blob.upload_from_file(my_file)
 
-#### Add upload photos
-encryption_key = "aa426195405adee2c8081bb9e7e74b19"
-file = "Eventbright Banner.jpg"
-##jpgfile = Image.open(file)
-blob = Blob(file, bucket)
-with open(file, "rb") as my_file:
-    blob.upload_from_file(my_file)
 
+
+jsonfile = 'Linguo-495a24a54222.json'
+s = StorageHandler(jsonfile)
+bucket = s.defineBucket()
+blobs = s.defineBlobs(bucket)
+s.downloadAllBlobs(blobs)
+file = input("Enter file name: ")
+s.uploadNewPhoto(file, bucket)
