51731766a69b3e534b8590096bf78aebfc17a398
jplaceres <33101350+jplaceres@users.noreply.github.com>
2019-03-24 08:33:58 -0400

    updates server storage

--- server/src/server.py
+++ server/src/server.py
@@ -8,8 +8,8 @@ class Server:
     def __init__(self, api_key_path):
         self.api_key_path = api_key_path
 
-        self.visionHandler = VisionHandler(api_key_path)
-        self.translationHandler = TranslationHandler(api_key_path)
+        #self.visionHandler = VisionHandler(api_key_path)
+        #self.translationHandler = TranslationHandler(api_key_path)
         self.storageHandler = StorageHandler(api_key_path)
 
     #method to get files from the bucket
@@ -19,33 +19,36 @@ class Server:
         blobs = self.storageHandler.getBlobs(bucket)
         self.storageHandler.downloadAllBlobs(blobs)
 
+        ######
+        oneFile = input("Enter one file to download: ")
+        self.storageHandler.downloadOneBlob(bucket, oneFile)
 
     # TODO: pass in imagePath, and language from a tuple
 
     # push the files from the get unprocessed files, processes the
-    def processImage(self, language):
-        # TODO: Step 1: Get list of all image? Maybe just one for now
-        imagePath = self.storageHandler.getSingleImage()
-
-        # Step 2: Get context for the image
-        labelInfo = self.visionHandler.process_image(imagePath)
-
-        # TODO: Step 3: Send context to web scraper for sentences
-
-        # TODO: Step 3.1: process the label info
-        processedLabels = labelInfo  # test code for now
-        translatedLabels = list()
-        # Step 4: Send sentence/context to translator
-        for label in processedLabels:
-            translatedLabels.add(translationHandler.translate(label, language))
-
-        # TODO: Step 5: Get overlay of image
-        overlayHandler.process_image(translatedLabels)
-
-        # TODO: Step 6: Output the image + text to speech translated text
-        self.storageHandler.putImage()
-        output.write(translatedLabels)
-
+##    def processImage(self, language):
+##        # TODO: Step 1: Get list of all image? Maybe just one for now
+##        imagePath = self.storageHandler.getSingleImage()
+##
+##        # Step 2: Get context for the image
+##        labelInfo = self.visionHandler.process_image(imagePath)
+##
+##        # TODO: Step 3: Send context to web scraper for sentences
+##
+##        # TODO: Step 3.1: process the label info
+##        processedLabels = labelInfo  # test code for now
+##        translatedLabels = list()
+##        # Step 4: Send sentence/context to translator
+##        for label in processedLabels:
+##            translatedLabels.add(translationHandler.translate(label, language))
+##
+##        # TODO: Step 5: Get overlay of image
+##        overlayHandler.process_image(translatedLabels)
+##
+##        # TODO: Step 6: Output the image + text to speech translated text
+##        self.storageHandler.putImage()
+##        output.write(translatedLabels)
+##
         # api-endpoint
         print("Unsupported Method")
 
@@ -64,4 +67,8 @@ class Server:
         print("Unsupported method")
 
 # TODO: have a file that provides the config for processing each image
+
+jsonfile = 'Linguo-495a24a54222.json'
+s = Server(jsonfile)
+s.getUnprocessedFiles()
 
